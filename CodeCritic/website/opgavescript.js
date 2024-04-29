5ce84d68fe59ad02fed0ee504e861de28c12d54b
MagnusLandkildehus <magland22@gmail.com>
2024-04-29 11:26:51 +0200

    Added:
    - Leaderboard preparation
    - Add assingment modal
    - Favicon test

--- CodeCritic/website/opgavescript.js
+++ CodeCritic/website/opgavescript.js
@@ -20,3 +20,29 @@ function submission() {
 
     document.getElementById("kildekode").value = "";
 }
+
+function updateLeaderboard() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1/leaderboard");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            data = xhr.response;
+            for (let i = 0; i < data.names.length; i++) {
+                // add to leaderboard
+            }
+        } else {
+            console.log("Leaderboard request failed");
+        }    
+    };
+}
+
+function addLine(pos, name, score, time) {
+    if (pos % 2 == 0) {
+        var background = "lightgrey";
+    } else {
+        var background = "white";
+    }
+    document.getElementById("table").innerHTML += `<tr style="background-color: ${background};" ><td>${name}</td><td>${score}</td><td>${time}</td><td>${pos}</td></tr>`;
+}
