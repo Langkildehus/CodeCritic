27f4eb705aa552ce1a3a435cf8251ef9e030632f
Sebastian Yde Junker <sebastianydejunker@gmail.com>
2024-05-04 20:58:32 +0200

    Made comments more readable

--- CodeCritic/Database.cpp
+++ CodeCritic/Database.cpp
@@ -1,13 +1,13 @@
 #include "pch.h"
 #include "database.h"
 
-//the database constructor
+// The database constructor
 Database::Database()
 {
-	//opens the database so it can be accessed
+	// Opens the database so it can be accessed
 	int exit = sqlite3_open(R"(Database.db)", &DB);
 
-	//creates a table in the data base for useres if it doesen't already exist (only needed if the database has been reset)
+	// Creates a table in the data base for useres if it doesen't already exist (only needed if the database has been reset)
 	std::string sql = "CREATE TABLE IF NOT EXISTS Users("
 		"ID			INTEGER PRIMARY KEY AUTOINCREMENT, "
 		"USERNAME   TEXT UNIQUE NOT NULL, "
@@ -15,17 +15,17 @@ Database::Database()
 	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, NULL);
 }
 
-//the database destructor
+// The database destructor
 Database::~Database()
 {
-	//closes the database
+	// Closes the database
 	sqlite3_close(DB);
 }
 
-//function to create a table for an assignment so there can be keept track of which users scores what
+// Function to create a table for an assignment so there can be keept track of which users scores what
 int Database::createAssignment(const std::string& tName)
 {
-	//makes a table in the database with the name from the input if it doesen't already exist
+	// Makes a table in the database with the name from the input if it doesen't already exist
 	char* messageError;
 	std::string sql;
 	sql = "CREATE TABLE IF NOT EXISTS " + tName + "("
@@ -37,13 +37,13 @@ int Database::createAssignment(const std::string& tName)
 
 		int exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
 
-		//checkes if the table is created correct if not it prints in the terminal what went wrong
+		// Checkes if the table is created correct if not it prints in the terminal what went wrong
 		if (exit != SQLITE_OK) 
 		{
 			std::cerr << "Error in createTable function\n" << messageError << "\n";
 			sqlite3_free(messageError);
 		}
-		//if it created the table correct prints in the terminal that it worked 
+		// If it created the table correct prints in the terminal that it worked 
 		else
 		{
 			std::cout << "Table created Successfully" << "\n";
@@ -51,17 +51,17 @@ int Database::createAssignment(const std::string& tName)
 	return 0;
 }
 
-//function to insert data into the table of the assignment that has been tried
+// Function to insert data into the table of the assignment that has been tried
 int Database::insertData(const std::string& tName, const std::string& username, const int Points, const ull time, const std::string& language)
 {
-	//inserts the scores a user gets if the user has not already a saved score
+	// Inserts the scores a user gets if the user has not already a saved score
 	sqlite3_stmt* stmt;
 	char* messageError;
 	std::string sql = "INSERT INTO " + tName + " (Username, Points, Time, Language) VALUES('" + username + "', '" + std::to_string(Points) + "', '" + std::to_string(time) + "', '" + language + "'); ";
 	
 	int exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
 
-	//if the user already has a scores in the database then try and update it
+	// If the user already has a scores in the database then try and update it
 	if (exit != SQLITE_OK) {
 		std::cerr << "Error in insertData function." << "\n" << messageError << "\n";
 		sqlite3_free(messageError);
@@ -83,10 +83,10 @@ int Database::insertData(const std::string& tName, const std::string& username, 
 		tempTime >> DBtime;
 		std::cout << DBpoints << "\n" << DBtime << "\n";
 
-		//check if the new score is better or the new score is the same and the time is better 
+		// Check if the new score is better or the new score is the same and the time is better 
 		if (DBpoints < Points || (DBpoints == Points && DBtime > time))
 		{
-			//then update the saved score
+			// Then update the saved score
 			sql = "UPDATE " + tName + " SET Points = '" + std::to_string(Points) + "', Time = '" + std::to_string(time) + "', Language = '" + language + "' WHERE Username = '" + username + "'; ";
 
 			int exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
@@ -100,7 +100,7 @@ int Database::insertData(const std::string& tName, const std::string& username, 
 				std::cout << "Records updated." << "\n";
 			return 0;
 		}
-		//if the new score is worse then the previous the do not update the saved score
+		// If the new score is worse then the previous the do not update the saved score
 		std::cout << "The records do not need to be updated" << "\n";
 		return 0;
 	}
@@ -109,22 +109,22 @@ int Database::insertData(const std::string& tName, const std::string& username, 
 		return 0;
 }
 
-//function to signup (create a new user)
+// Function to signup (create a new user)
 bool Database::signup(const std::string& username, const std::string& password)
 {
-	//makes a new user with the username and password from the input if there is not already a user with the same username
+	// Makes a new user with the username and password from the input if there is not already a user with the same username
 	char* messageError;
 	std::string sql("INSERT INTO Users (USERNAME, PASSWORD) VALUES('"+ username + "','" + password +"');");
 
 	int exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
 
-	//checks if the worked other wises prints what went wrong in the terminal and returns that it did not work
+	// Checks if the worked other wises prints what went wrong in the terminal and returns that it did not work
 	if (exit != SQLITE_OK) {
 		std::cerr << "Error in insertUserData function." << "\n" << messageError << "\n";
 		sqlite3_free(messageError);
 		return false;
 	}
-	//returns that it worked and prints in the terminal that it worked
+	// Returns that it worked and prints in the terminal that it worked
 	else
 	{
 		std::cout << "Records inserted Successfully!" << "\n";
@@ -132,24 +132,27 @@ bool Database::signup(const std::string& username, const std::string& password)
 	}
 }
 
-//function to login (access a user that already exists)
+// Function to login (access a user that already exists)
 bool Database::checkLogin(const std::string& username, const std::string& Password)
 {
-	//to login the password you try to login with and the login that the user has need to be the same
-	//gets the password from the database
+	// To login the password you try to login with and the login that the user has need to be the same 
+
+	// Gets the password from the database
 	sqlite3_stmt* stmt;
 	std::string sql = "SELECT PASSWORD FROM Users WHERE USERNAME= '" + username + "';";
 	int exit = sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
 
-	//check if it was possible to get the password from the database. 
-	//if not, print the the error to the terminal and return false to indicate that the login failed
+	// Check if it was possible to get the password from the database.
+
+	// If not, print the the error to the terminal and return false to indicate that the login failed
 	if (exit != SQLITE_OK|| sqlite3_step(stmt) != SQLITE_ROW)
 	{
 		std::cout << "ERROR:" << sqlite3_errmsg(DB) << "\n";
 		return false;
 	}
-	//then check if the password from the database is the same as the one that the user tried
-	//if it is the same then return that the login worked otherwise return that it did not work
+	// Then check if the password from the database is the same as the one that the user tried
+	
+	// If it is the same then return that the login worked otherwise return that it did not work
 	if (Password == std::string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0))))
 	{
 		std::cout << "win\n";
@@ -159,30 +162,29 @@ bool Database::checkLogin(const std::string& username, const std::string& Passwo
 	return false;
 }
 
-//function to get a leaderboard over which useres made the best and fastest solution
+// Function to get a leaderboard over which useres made the best and fastest solution
 std::string Database::Assigmentleaderboard(const std::string tName) 
 {
-	//get the users and scores from the given assigment
-	//in order of which users that got most points and used least amount of time
+	// Get the users and scores from the given assigment
+	// in order of which users that got most points and used least amount of time
 	sqlite3_stmt* stmt;
 	std::string sql = "SELECT Username, Points, Time, Language FROM " + tName + " ORDER BY Points DESC, Time ASC;";
 	std::string str = "[";
 	int exit = sqlite3_prepare_v2(DB, sql.c_str(), -1, &stmt, NULL);
 
-	//Go through all the users and scores one by one and write them in a string that follows a JSON format 
-	//so it can be sent to the http server
+	// Go through all the users and scores one by one and write them in a string that follows a JSON format 
+	// so it can be sent to the web server
 	while(true)
 	{
-		//check if there is more scores left 
+		// Check if there is more scores left 
 		if (exit != SQLITE_OK || sqlite3_step(stmt) != SQLITE_ROW)
 		{
-			//if there is no more scores left then add the end of a JSON file to it
-			//and return it
+			// If there is no more scores left then add the end of a JSON file to it, and return it
 			str[str.size() - 1] = ']';
 			std::cout << str << "\n";
 			return str;
 		}
-		//write the user and the users scores in a string so it follow JSON format
+		// Write the user and the users scores in a string so it follow JSON format
 		str += '{';
 
 		str += "\"name\": \"" + std::string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0))) + "\",";
